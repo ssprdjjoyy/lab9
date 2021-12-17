4d568954e5c837c822343a9ea2857e477602e6f9
sprdnvan1 <nariyasprdnva@mail.ru>
2021-12-17 22:32:33 +0900

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main (String[] args) {
+        Auto myAuto1  = new Auto("Ford", 180);
+        System.out.println(myAuto1.getFirm() + " " + myAuto1.getMaxSpeed());
+    }
+}
